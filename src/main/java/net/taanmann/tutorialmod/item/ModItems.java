929fa39e7dfe24498c13f73d87e74998d84e823e
tannerjackley <s561664@nwmissouri.edu>
2025-07-06 22:35:28 -0500

    Add 'ChiselItem'

--- src/main/java/net/taanmann/tutorialmod/item/ModItems.java
+++ src/main/java/net/taanmann/tutorialmod/item/ModItems.java
@@ -6,6 +6,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.taanmann.tutorialmod.TutorialMod;
+import net.taanmann.tutorialmod.item.custom.ChiselItem;
+import org.checkerframework.checker.units.qual.C;
 
 public class ModItems {
 
@@ -15,6 +17,8 @@ public class ModItems {
     public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite", () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel", () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
